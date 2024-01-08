bccfe0ef7a60713b141dd3fda6f284855fd5d139
Xiaohui Qu <xq@earth.org>
2024-01-07 20:23:29 -0500

    set number of gpus per node

--- rankaae/cmd/train.py
+++ rankaae/cmd/train.py
@@ -134,6 +134,8 @@ def main():
     if args.processes > 1:
         mpi_cmd = train_config.get("mpi_cmd", "srun")
         ipp.cluster.launcher.MPILauncher.mpi_cmd = [mpi_cmd]
+        ipp.cluster.launcher.MPILauncher.mpi_args = [
+            "--gpus-per-node", torch.cuda.device_count()]
         ip = socket.gethostbyname(socket.gethostname())
         with ipp.Cluster(engines=RankAAEMPIEngineSetLauncher, n=args.processes,
                          controller_ip='*', controller_location=ip, 
