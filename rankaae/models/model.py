62a8b3b30af1985480ba07c5591b3dc5a5618261
Xiaohui Qu <xq@earth.org>
2024-02-27 09:42:33 -0500

    Revert "reverse the order of activation function and batch normalization"
    This reverts commit 775218b37f4e31e7f672638f9511354b5c886696.

--- rankaae/models/model.py
+++ rankaae/models/model.py
@@ -108,17 +108,16 @@ class FCEncoder(nn.Module):
         super(FCEncoder, self).__init__()
 
         self.dim_in = dim_in
-        sequential_layers = [nn.Linear(dim_in, hidden_size, bias=True)] # first layer
+        sequential_layers = [nn.Linear(dim_in, hidden_size, False)] # first layer
         for _ in range(n_layers-2):
             sequential_layers.extend([
+                nn.BatchNorm1d(hidden_size, affine=True),
                 Swish(num_parameters=hidden_size, init=1.0),
-                nn.BatchNorm1d(hidden_size, affine=False),
-                nn.Linear(hidden_size, hidden_size, bias=True)])
+                nn.Linear(hidden_size, hidden_size, bias=False)])
         sequential_layers.extend([ # last layer
+            nn.BatchNorm1d(hidden_size, affine=True),
             Swish(num_parameters=hidden_size, init=1.0),
-            nn.BatchNorm1d(hidden_size, affine=False),
-            nn.Linear(hidden_size, nstyle, bias=True),
-            Swish(num_parameters=nstyle, init=1.0),
+            nn.Linear(hidden_size, nstyle, bias=False),
             nn.BatchNorm1d(nstyle, affine=False)])
             # add this batchnorm layer to make sure the output is standardized.
         self.main = nn.Sequential(*sequential_layers)
@@ -165,15 +164,15 @@ class FCDecoder(nn.Module):
 
         ll_act = build_activation_function(dim_out, last_layer_activation)
 
-        sequential_layers = [nn.Linear(nstyle, hidden_size, bias=True)] # the first layer.
+        sequential_layers = [nn.Linear(nstyle, hidden_size, bias=False)] # the first layer.
         for _ in range(n_layers-2):
             sequential_layers.extend([ # the n layers in the middle
+                nn.BatchNorm1d(hidden_size, affine=True),
                 Swish(num_parameters=hidden_size, init=1.0),
-                nn.BatchNorm1d(hidden_size, affine=False),
-                nn.Linear(hidden_size, hidden_size, bias=True)])
+                nn.Linear(hidden_size, hidden_size, bias=False)])
         sequential_layers.extend([ # the last layer
+            nn.BatchNorm1d(hidden_size, affine=True),
             Swish(num_parameters=hidden_size, init=1.0),
-            nn.BatchNorm1d(hidden_size, affine=False),
             nn.Linear(hidden_size, dim_out),
             ll_act])  
         self.main = nn.Sequential(*sequential_layers)
@@ -219,14 +218,18 @@ class ExLayers(nn.Module):
 
         pe = torch.arange(dim_out, dtype=torch.float32, requires_grad=False) + 1
         self.register_buffer("position_embedding", pe[:, None])
-        gate_layers = [nn.Linear(1, gate_window, bias=True)]
-        assert n_gate_layers >= 1
-        for _ in range(n_gate_layers-1):
+        gate_layers = [nn.Linear(1, gate_window, bias=False)]
+        assert n_gate_layers >= 2
+        for _ in range(n_gate_layers-2):
             gate_layers.extend([
+                nn.BatchNorm1d(gate_window, affine=True),
                 Swish(num_parameters=gate_window, init=1.0),
-                nn.BatchNorm1d(gate_window, affine=False),
-                nn.Linear(gate_window, gate_window, bias=True)])
-        gate_layers.append(nn.Softmax(dim=1))  
+                nn.Linear(gate_window, gate_window, bias=False)])
+        gate_layers.extend([
+            nn.BatchNorm1d(gate_window, affine=True),
+            Swish(num_parameters=gate_window, init=1.0),
+            nn.Linear(gate_window, gate_window, bias=True),
+            nn.Softmax(dim=1)])  
         self.gate_weights = nn.Sequential(*gate_layers)
         uw = torch.eye(gate_window, dtype=torch.float32, requires_grad=False)[:, None, :]
         self.register_buffer('upend_weights', uw)
@@ -238,18 +241,18 @@ class ExLayers(nn.Module):
                           padding=self.padding, padding_mode=self.padding_mode)]
         else:
             intensity_layers = [
-                nn.Conv1d(1, n_channels, kernel_size=hidden_kernel_size, bias=True,
+                nn.Conv1d(1, n_channels, kernel_size=hidden_kernel_size, bias=False,
                           padding=self.padding, padding_mode=self.padding_mode)]
         for _ in range(n_exlayers - 2):
             intensity_layers.extend([
+                nn.BatchNorm1d(n_channels, affine=True),
                 Swish(num_parameters=n_channels, init=1.0),
-                nn.BatchNorm1d(n_channels, affine=False),
-                nn.Conv1d(n_channels, n_channels, kernel_size=hidden_kernel_size, bias=True,
+                nn.Conv1d(n_channels, n_channels, kernel_size=hidden_kernel_size, bias=False,
                           padding=self.padding, padding_mode=self.padding_mode)])
         if n_exlayers >= 2:
             intensity_layers.extend([
+                nn.BatchNorm1d(n_channels, affine=True),
                 Swish(num_parameters=n_channels, init=1.0),
-                nn.BatchNorm1d(n_channels, affine=False),
                 nn.Conv1d(n_channels, 1, kernel_size=hidden_kernel_size, bias=True,
                           padding=self.padding, padding_mode=self.padding_mode)])
         if last_layer_activation:
@@ -338,15 +341,15 @@ class DiscriminatorFC(nn.Module):
     def __init__(self, hiden_size=64, nstyle=5, noise=0.1, layers=3):
         super(DiscriminatorFC, self).__init__()
         
-        sequential_layers = [nn.Linear(nstyle, hiden_size, bias=True)]
+        sequential_layers = [nn.Linear(nstyle, hiden_size, bias=False)]
         for _ in range(layers-2):
             sequential_layers.extend([
+                nn.BatchNorm1d(hiden_size, affine=True),
                 Swish(num_parameters=hiden_size, init=1.0),
-                nn.BatchNorm1d(hiden_size, affine=False),
-                nn.Linear(hiden_size, hiden_size, bias=True)])
+                nn.Linear(hiden_size, hiden_size, bias=False)])
         sequential_layers.extend([
+            nn.BatchNorm1d(hiden_size, affine=True),
             Swish(num_parameters=hiden_size, init=1.0),
-            nn.BatchNorm1d(hiden_size, affine=False),
             nn.Linear(hiden_size, 1, bias=True)])
         self.main = nn.Sequential(*sequential_layers)
         
