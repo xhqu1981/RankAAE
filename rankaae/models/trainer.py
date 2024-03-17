5b866b45544e46978bb423b0481a5beca4abc2bc
Xiaohui Qu <xq@earth.org>
2024-03-17 13:40:58 -0400

    specific device

--- rankaae/models/trainer.py
+++ rankaae/models/trainer.py
@@ -391,8 +391,8 @@ class Trainer:
                 callback(epoch, metrics)
 
         if self.__dict__.get('swa_start', -1) > 0:        
-            torch.optim.swa_utils.update_bn(self.train_loader, self.swa_encoder)
-            torch.optim.swa_utils.update_bn(self.train_loader, self.swa_decoder)
+            torch.optim.swa_utils.update_bn(self.train_loader, self.swa_encoder, device=self.device)
+            torch.optim.swa_utils.update_bn(self.train_loader, self.swa_decoder, device=self.device)
             model_dict['Encoder'] = self.swa_encoder.module
             model_dict['Decoder'] = self.swa_encoder.module
             model_dict['NonSWA_Encoder'] = self.encoder
