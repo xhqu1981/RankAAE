6939623a8b5916622b3e5f286a6e0556cc41d3d2
Xiaohui Qu <xq@earth.org>
2024-03-19 18:28:47 -0400

    set base lr to a very small value

--- rankaae/models/trainer.py
+++ rankaae/models/trainer.py
@@ -387,6 +387,7 @@ class Trainer:
             else:
                 if epoch == self.__dict__.get('swa_start', -1):
                     self.optimizer_name = 'SGD'
+                    self.lr_base = 1.0E-10
                     self.load_optimizers()
                     self.load_schedulers()
                 self.swa_ae.update_parameters(self.orig_ae)
