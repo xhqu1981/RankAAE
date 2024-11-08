46b6112ef3b2419fdf5d549dedaf67788c1b2b3e
Xiaohui Qu <xq@earth.org>
2024-11-08 15:10:20 -0500

    adapt transformer to be batch first

--- rankaae/models/transformer.py
+++ rankaae/models/transformer.py
@@ -20,7 +20,7 @@ class PositionalEncoding(nn.Module):
         >>> pos_encoder = PositionalEncoding(d_model)
     """
 
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, d_model, dropout=0.0, max_len=5000, batch_first=True):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
@@ -29,7 +29,9 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = pe.unsqueeze(0)
+        if not batch_first:
+            pe = pe.transpose(0, 1)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
@@ -42,43 +44,31 @@ class PositionalEncoding(nn.Module):
         Examples:
             >>> output = pos_encoder(x)
         """
-
-        x = x + self.pe[:x.size(0), :]
+        x = x + self.pe
         return self.dropout(x)
 
-class TransformerModel(nn.Transformer):
-    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
-        super(TransformerModel, self).__init__(d_model=ninp, nhead=nhead, dim_feedforward=nhid, 
-                                               num_encoder_layers=nlayers)
-        self.src_mask = None
-        self.pos_encoder = PositionalEncoding(ninp, dropout)
+class TransformerEnergyPositionPredictor(nn.Module):
+    def __init__(self, n_grid, d_model, nhead, dim_feedforward, nlayers, 
+                 dropout=0.0, batch_first=True, activation='relu'):
+        super(TransformerEnergyPositionPredictor, self).__init__()
+        self.pos_encoder = PositionalEncoding(d_model, dropout, 
+                                              max_len=n_grid, batch_first=batch_first)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=512, 
+                                                   nhead=nhead, 
+                                                   dim_feedforward=dim_feedforward,
+                                                   dropout=dropout,
+                                                   activation=activation,
+                                                   batch_first=batch_first)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=nlayers)
+        self.input_emb = nn.Linear(1, d_model)
+        self.d_model = d_model
+        self.decoder = nn.Linear(d_model, 1)
 
-        self.input_emb = nn.Embedding(ntoken, ninp)
-        self.ninp = ninp
-        self.decoder = nn.Linear(ninp, ntoken)
 
-        self.init_weights()
-
-    def _generate_square_subsequent_mask(self, sz):
-        return torch.log(torch.tril(torch.ones(sz,sz)))
-
-    def init_weights(self):
-        initrange = 0.1
-        nn.init.uniform_(self.input_emb.weight, -initrange, initrange)
-        nn.init.zeros_(self.decoder.bias)
-        nn.init.uniform_(self.decoder.weight, -initrange, initrange)
-
-    def forward(self, src, has_mask=True):
-        if has_mask:
-            device = src.device
-            if self.src_mask is None or self.src_mask.size(0) != len(src):
-                mask = self._generate_square_subsequent_mask(len(src)).to(device)
-                self.src_mask = mask
-        else:
-            self.src_mask = None
-
-        src = self.input_emb(src) * math.sqrt(self.ninp)
+    def forward(self, spec):
+        src = self.input_emb(spec) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
-        output = self.encoder(src, mask=self.src_mask)
-        output = self.decoder(output)
-        return F.log_softmax(output, dim=-1)
+        output = self.encoder(src)
+        output: torch.FloatTensor = self.decoder(output)
+        ene_pos = output.squeeze(dim=1)
+        return ene_pos
